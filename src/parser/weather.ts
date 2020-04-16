0dd3f39ad58ee7b5e21c4d9af00119b77eb31a90
Hyunkook Park <36787678+NaLDo627@users.noreply.github.com>
2020-04-16 17:29:31 +0900

    edit weather.ts for test

--- src/parser/weather.ts
+++ src/parser/weather.ts
@@ -6,8 +6,9 @@ import weatherData from './data/weather.json';
 
 export const parse = async() => {
   const token = process.env.WEATHER_API_KEY;
-  const city = 'Busan';
+  const city = 'Seongnam-si';
 
+  /*
   const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token}&units=metric`);
   const data = response.data;
 
@@ -16,5 +17,10 @@ export const parse = async() => {
   return {
     weather: (<any> weatherData)[data.weather[0].id],
     temp: `(${data.main.temp_min}도 ~ ${data.main.temp_max}도)`
+  };
+  */
+  return {
+    weather: "짱짱맑음",
+    temp: `(10도 ~ 20도)`
   };
 };
